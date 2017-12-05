3bd36aa16e73a26d838a3b489725c0596e237822
renzhentaxibaerde <renzhentaxibaerde@gmail.com>
2017-12-05 12:41:19 -0500

    Remove the injection of grades. It creates ambiguity when implementing several methods of assignment.

--- Main/src/main/java/model/assignments/classes/Assignment.java
+++ Main/src/main/java/model/assignments/classes/Assignment.java
@@ -19,14 +19,12 @@ public class Assignment implements IAssignment
      * @param name the name of the assignment. This should be unique within a course
      * @param description the description of the assignment
      * @param course the course that contains this assignment.
-     * @param grades the grades. for every student in the roster, there is one entry in grades that is associated with them.
      */
-    public Assignment(String name, String description, ICourse course, Map<IAccount,Float> grades)
+    public Assignment(String name, String description, ICourse course)
     {
         this.name = name;
         this.description = description;
         this.course = course;
-        this.grades = grades;
     }
 
     /**
