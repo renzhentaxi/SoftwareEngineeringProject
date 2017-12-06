ac5df6e50dfda460035677d52b0dcbb1d7a8206e
renzhentaxibaerde <renzhentaxibaerde@gmail.com>
2017-12-05 20:13:13 -0500

    Renamed to InvalidLoginException

---
+++ Main/src/main/java/services/login/exceptions/InvalidLoginException.java
@@ -0,0 +1,4 @@
+package services.login.exceptions;
+
+public class InvalidLoginException extends RuntimeException{
+}
--- Main/src/main/java/services/login/exceptions/InvalidLoginTokenException.java
+++
@@ -1,4 +0,0 @@
-package services.login.exceptions;
-
-public class InvalidLoginTokenException extends RuntimeException{
-}
