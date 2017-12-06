a8e376994a782a8617da5d0308b614c548baf0d4
renzhentaxibaerde <renzhentaxibaerde@gmail.com>
2017-12-05 21:00:00 -0500

    Added hasAccount, hasCourse method

--- Main/src/main/java/services/storage/model/Catalog.java
+++ Main/src/main/java/services/storage/model/Catalog.java
@@ -13,8 +13,8 @@ public class Catalog
     /**
      * finds and returns the course with the courseId
      *
-     * @param requester the user who attempts to get the course
-     * @param courseName  the id of the course
+     * @param requester  the user who attempts to get the course
+     * @param courseName the id of the course
      * @return the course
      * @throws CourseDoesNotExistException if there is no course with the id in the system
      * @throws NoPermissionException       if the requester does not belong to the course
@@ -29,7 +29,7 @@ public class Catalog
      * finds and returns the account with the given accountId
      *
      * @param requester the user who attempts to get the account
-     * @param userName the id of the account
+     * @param userName  the id of the account
      * @return the account
      * @throws AccountDoesNotExistException if there is no account with the id in the system
      * @throws NoPermissionException        if the requester is not a system account
@@ -37,5 +37,43 @@ public class Catalog
     public static IAccount getAccount(ILoginToken requester, String userName)
     {
         throw new NotImplementedException();
+    }
+
+    /**
+     * checks if account exist
+     *
+     * @param requester the user who requests this action
+     * @param userName the name of the account to check
+     * @return true if it exist, false otherwise
+     */
+    public static boolean hasAccount(ILoginToken requester, String userName)
+    {
+        try
+        {
+            getAccount(requester, userName);
+        } catch (AccountDoesNotExistException e)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * checks if course exist
+     *
+     * @param requester the user who requests this action
+     * @param courseName the name of the course to check
+     * @return true if it exist, false otherwise
+     */
+    public static boolean hasCourse(ILoginToken requester, String courseName)
+    {
+        try
+        {
+            getCourse(requester, courseName);
+        } catch (CourseDoesNotExistException e)
+        {
+            return false;
+        }
+        return true;
     }
 }
