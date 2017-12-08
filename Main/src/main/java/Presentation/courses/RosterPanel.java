6d9b52008ca4f2d7c0a26a31c778fd90c85e92dc
Han Cho <hankyolcho@mail.adelphi.edu>
2017-12-08 17:09:13 -0500

    working on roster

--- Main/src/main/java/Presentation/courses/RosterPanel.java
+++ Main/src/main/java/Presentation/courses/RosterPanel.java
@@ -2,12 +2,51 @@ package Presentation.courses;
 
 import Presentation.base.App;
 import Presentation.base.MainPanel;
+import model.courses.classes.Roster;
 import services.login.interfaces.ILoginToken;
+import java.util.List;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class RosterPanel extends MainPanel
 {
+    private List<Roster> rosters;
+
     public RosterPanel(App app, ILoginToken token, MainPanel prevPanel)
     {
         super(app, token, prevPanel);
+        //this.rosters = userAccount.(token);
+        assemble();
+    }
+
+    private GridBagConstraints constraint;
+
+    @Override
+    public void assemble()
+    {
+        super.assemble();
+        clearMenu();
+        constraint = new GridBagConstraints();
+        setLayout(new GridBagLayout());
+
+        //name field
+        JLabel rosterLabel = new JLabel("List of rosters:");
+        add(rosterLabel, 0, 0, 10);
+
+        /*Description field
+        JLabel assignDescLabel = new JLabel("Assignment Description:");
+        assignmentDescTextField = new JTextArea(40,40);
+        add(assignDescLabel, 0, 2, 2);
+        add(assignmentDescTextField, 0, 3, 2);
+        */
+    }
+
+    private void add(Component component, int x, int y, int width)
+    {
+        constraint.gridx = x;
+        constraint.gridy = y;
+        constraint.gridwidth = width;
+        add(component, constraint);
     }
 }
